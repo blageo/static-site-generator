62835cf2ce963801873bc76b4c1876f8821830d5
blageo <blageo@proton.me>
2025-04-06 20:34:27 -0400

    added basic split_delimiter function and tests, need more unittesting

---
+++ src/test_splitnodesdelimiter.py
@@ -0,0 +1,34 @@
+import unittest
+from textnode import TextNode, TextType
+from splitnodesdelimiter import split_nodes_delimiter
+
+class TestSplitNodesDelimiter(unittest.TestCase):
+    def test_split_nodes_delimiter_code(self):
+        node = TextNode("This is text with a `code block` word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertEqual(new_nodes, [
+            TextNode("This is text with a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" word", TextType.TEXT),
+        ])
+
+    def test_split_nodes_delimiter_bold(self):
+        node = TextNode("This is text with a **bold** word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertEqual(new_nodes, [
+            TextNode("This is text with a ", TextType.TEXT),
+            TextNode("bold", TextType.BOLD),
+            TextNode(" word", TextType.TEXT),
+        ])
+
+    def test_split_nodes_delimiter_italic(self):
+        node = TextNode("This is text with a _italically emphasized_ and other stuff", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "_", TextType.ITALIC)
+        self.assertEqual(new_nodes, [
+            TextNode("This is text with a ", TextType.TEXT),
+            TextNode("italically emphasized", TextType.ITALIC),
+            TextNode(" and other stuff", TextType.TEXT),
+        ])
+
+if __name__ == "__main__":
+    unittest.main()
